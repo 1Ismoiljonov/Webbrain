771aac248a16cc34d71735c6846f7d89d3bcdc58
1Ismoiljonov <99031003+1Ismoiljonov@users.noreply.github.com>
2023-01-30 10:44:23 +0500

    test

--- node.js
+++ node.js
@@ -1,1 +1,29 @@
-console.log("samandar");
+// console.log ni ishlashi
+
+// console.log("samandar1");
+// console.log("samandar2");
+// console.log("samandar3");
+
+// varibles  = let var const => o'zgaruvchilar
+
+// let text = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius, pariatur.'
+// console.log(text);
+// console.log(text);
+
+// let num;   => decloration
+// num = 1   =>  assign
+// console.log(num);
+
+
+// let num1 = 1
+// let num2 = num1
+// console.log(num1, num2);
+// let names =  'Samandar'
+// let surname = 'Ismoiljonov'
+// console.log(`${surname} ${names}`);
+
+// boolean => bu true yoki false qiymatlarni qabul qiladi
+
+let num1 = 1
+let num2 = 2
+console.log(num1 < num2);
